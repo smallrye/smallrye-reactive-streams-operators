b1b925384987cc840c30bc2a02f62a73b2b11b2f
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-05 10:35:08 +0100

    Cleanup the branch to store the web site

--- tck/src/test/java/io/smallrye/reactive/streams/tck/InContainerIT.java
+++
@@ -1,7 +0,0 @@
-package io.smallrye.reactive.streams.tck;
-
-import org.eclipse.microprofile.reactive.streams.tck.arquillian.ReactiveStreamsArquillianTck;
-
-public class InContainerIT extends ReactiveStreamsArquillianTck {
-
-}
