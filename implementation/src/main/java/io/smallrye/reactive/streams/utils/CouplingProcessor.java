379b3678310e89d0db215ab6d94da3060e1b779a
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-21 09:30:02 +0100

    Implement the "coupled" operator

---
+++ implementation/src/main/java/io/smallrye/reactive/streams/utils/CouplingProcessor.java
@@ -0,0 +1,27 @@
+package io.smallrye.reactive.streams.utils;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+public class CouplingProcessor<I, O> implements Publisher<O> {
+
+    private final SubscriptionObserver<I> controller;
+    private final Publisher<O> publisher;
+
+    public CouplingProcessor(Publisher<I> source, Subscriber<I> subscriber, Publisher<O> publisher) {
+        controller = new SubscriptionObserver<>(source, subscriber);
+        this.publisher = publisher;
+        controller.run();
+    }
+
+
+    @Override
+    public synchronized void subscribe(Subscriber<? super O> subscriber) {
+        SubscriptionObserver<O> observer = new SubscriptionObserver<>(this.publisher, subscriber);
+        controller.setObserver(observer);
+        observer.setObserver(controller);
+        observer.run();
+    }
+
+
+}
